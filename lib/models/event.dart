662d379de33622186b4a4587aa33db6701fb1bba
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-01-17 22:10:17 +0530

    event model done

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,13 +1,29 @@
+import 'dart:html';
+
 class Event {
-  final int event_id;
-  final String event_name;
+  final int eventId;
+  final String eventName;
   final String description;
-  final int amount;
+  final double eventAmount;
+  final List<String> eventImages;
+  final DateTime publishedOn;
+  final int noOfParticipants;
+  final String eventLink;
+  final String contactPerson;
+  final int contactNo;
+  final DateTime registartionDeadline;
 
   Event({
-    required this.event_id,
-    required this.event_name,
+    required this.eventId,
+    required this.eventName,
     required this.description,
-    required this.amount,
+    required this.publishedOn,
+    required this.eventImages,
+    required this.eventLink,
+    required this.eventAmount,
+    required this.contactPerson,
+    required this.contactNo,
+    required this.noOfParticipants,
+    required this.registartionDeadline,
   });
 }
