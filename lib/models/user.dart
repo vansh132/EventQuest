4ca7b285cc28f44ebd02955805c82b9dcd64afb0
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-01-17 22:35:51 +0530

    announcement model created.

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String id;
+  final String userName;
+  final String password;
+  final String userType;
+
+  User(
+      {required this.id,
+      required this.userName,
+      required this.password,
+      required this.userType});
+  // final String token;
+}
