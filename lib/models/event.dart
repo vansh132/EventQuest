97ba5bdcf36671646e0bcc764d0e2437b1ed5f2d
vansh132 <shah.vansh132@gmail.com>
2024-01-17 22:24:06 +0530

    homescreen - recent event structure added

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 class Event {
   final String eventId;
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eventquest/models/event.dart';
 import 'package:eventquest/screen_items/highlights.dart';
 import 'package:eventquest/widgets/top_bar.dart';
 import 'package:eventquest/widgets/user_info.dart';
@@ -8,6 +9,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Event> events = [
+      Event(
+        eventId: "abc132",
+        eventName: "Talent Show",
+        description: "Fresher's can show their talent",
+        publishedOn: DateTime.now(),
+        eventImages: [],
+        eventLink: "",
+        eventAmount: 200,
+        contactPerson: "Helen K Joy",
+        contactNo: 9099897859,
+        noOfParticipants: 2,
+        registartionDeadline: DateTime(2024, 1, 31),
+      ),
+    ];
+
     return Scaffold(
       body: Container(
         color: Colors.amber,
@@ -16,9 +33,43 @@ class HomeScreen extends StatelessWidget {
             TopBar(),
             UserBar(),
             Highlights(),
+            RecentEvents(context, events),
           ],
         ),
       ),
     );
   }
+}
+
+Widget RecentEvents(BuildContext context, List<Event> events) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    color: Colors.deepOrange,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Recent Events"),
+        SizedBox(
+          height: 12,
+        ),
+        Container(
+          height: 200,
+          child: GridView.builder(
+            scrollDirection: Axis.horizontal,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: events.length,
+            itemBuilder: (context, index) => EventItem(events[index]),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget EventItem(Event event) {
+  return Text(event.eventName);
 }
