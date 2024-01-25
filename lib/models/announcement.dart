b6d33d1eec1d422021ab325072f83cd793a64b09
vansh132 <shah.vansh132@gmail.com>
2024-01-25 11:54:05 +0530

    home screen - structure ready

--- lib/models/announcement.dart
+++ lib/models/announcement.dart
@@ -2,10 +2,14 @@ class Announcement {
   final String title;
   final String description;
   final List<String>? image;
+  final DateTime publishedOn;
+  final String publishedBy;
 
   Announcement({
     required this.title,
     required this.description,
     this.image,
+    required this.publishedBy,
+    required this.publishedOn,
   });
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,50 @@
+import 'package:eventquest/models/announcement.dart';
 import 'package:eventquest/models/event.dart';
 import 'package:eventquest/screen_items/highlights.dart';
+import 'package:eventquest/screens/detailed_screens/event_detail_screen.dart';
 import 'package:eventquest/widgets/top_bar.dart';
 import 'package:eventquest/widgets/user_info.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Announcement> announcements = [
+      Announcement(
+        title: 'Placement',
+        description:
+            'Heartiest congratulations to all the brilliant students who have achieved remarkable success and secured placements! Your hard work, dedication, and exceptional skills have paved the way for this significant accomplishment. Wishing you a future filled with continued success, growth, and prosperity. Well done!Heartiest congratulations to all the brilliant students who have achieved remarkable success and secured placements! Your hard work, dedication, and exceptional skills have paved the way for this significant accomplishment. Wishing you a future filled with continued success, growth, and prosperity. Well done!Heartiest congratulations to all the brilliant students who have achieved remarkable success and secured placements! Your hard work, dedication, and exceptional skills have paved the way for this significant accomplishment. Wishing you a future filled with continued success, growth, and prosperity. Well done!',
+        image: [
+          "https://media.istockphoto.com/id/514363071/photo/just-one-moment-please.webp?b=1&s=170667a&w=0&k=20&c=8xY02WALsH-RYsCz83EBigqH4z3RQg76ZfOhgQjlefU=",
+        ],
+        publishedBy: "Helen K Joy",
+        publishedOn: DateTime(2024, 1, 23),
+      ),
+      Announcement(
+        title: 'ESE Results',
+        description:
+            'Heartiest congratulations to all the brilliant students who have achieved remarkable success and secured placements! Your hard work, dedication, and exceptional skills have paved the way for this significant accomplishment. Wishing you a future filled with continued success, growth, and prosperity. Well done!Heartiest congratulations to all the brilliant students who have achieved remarkable success and secured placements! Your hard work, dedication, and exceptional skills have paved the way for this significant accomplishment. Wishing you a future filled with continued success, growth, and prosperity. Well done!Heartiest congratulations to all the brilliant students who have achieved remarkable success and secured placements! Your hard work, dedication, and exceptional skills have paved the way for this significant accomplishment. Wishing you a future filled with continued success, growth, and prosperity. Well done!',
+        image: [
+          "https://media.istockphoto.com/id/514363071/photo/just-one-moment-please.webp?b=1&s=170667a&w=0&k=20&c=8xY02WALsH-RYsCz83EBigqH4z3RQg76ZfOhgQjlefU=",
+        ],
+        publishedBy: "Helen K Joy",
+        publishedOn: DateTime(2024, 1, 23),
+      ),
+      Announcement(
+        title: 'Nexus - Fest 2023',
+        description:
+            'Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!Kudos to the festival winners for their remarkable achievements! Your dedication and talent have shone brightly, inspiring and captivating everyone. Heartfelt congratulations on your well-deserved success and recognition!',
+        image: [
+          "https://images.unsplash.com/photo-1658581872509-c8d19777bd24?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "https://media.istockphoto.com/id/514363071/photo/just-one-moment-please.webp?b=1&s=170667a&w=0&k=20&c=8xY02WALsH-RYsCz83EBigqH4z3RQg76ZfOhgQjlefU="
+        ],
+        publishedBy: "Dr. Sudhakar T",
+        publishedOn: DateTime(2024, 1, 10),
+      ),
+    ];
     List<Event> events = [
       Event(
         eventCategory: "UG",
@@ -150,17 +186,54 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         color: Colors.amber,
-        child: Column(
-          children: [
-            TopBar(),
-            UserBar(),
-            Highlights(),
-            RecentEvents(context, events),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TopBar(),
+              UserBar(),
+              Highlights(),
+              RecentEvents(context, events),
+              RecentAnnouncements(context, announcements),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget RecentAnnouncements(
+    BuildContext context, List<Announcement> announcements) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    padding: const EdgeInsets.all(16),
+    color: Colors.greenAccent,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Recent Announcements",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Container(
+          color: Colors.greenAccent,
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: announcements.length,
+            itemBuilder: (context, index) =>
+                AnnouncementItem(context, announcements[index]),
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 Widget RecentEvents(BuildContext context, List<Event> events) {
@@ -192,86 +265,179 @@ Widget RecentEvents(BuildContext context, List<Event> events) {
               mainAxisSpacing: 4.0,
             ),
             itemCount: events.length,
-            itemBuilder: (context, index) => EventItem(events[index]),
+            itemBuilder: (context, index) => EventItem(events[index], context),
           ),
-        )
+        ),
       ],
     ),
   );
 }
 
-Widget EventItem(Event event) {
+Widget AnnouncementItem(BuildContext context, Announcement announcement) {
   return Container(
+    // width: 180,
     margin: const EdgeInsets.all(8),
     padding: const EdgeInsets.all(8),
     decoration: BoxDecoration(
-      image: const DecorationImage(
-        image: NetworkImage(
-          "https://images.unsplash.com/photo-1521048525864-2130780a12a5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        ),
-        fit: BoxFit.cover,
-      ),
-      // gradient: LinearGradient(
-      //   begin: Alignment.topLeft, // Gradient direction
-      //   end: Alignment.bottomRight,
-      //   colors: [
-      //     Colors.amberAccent, // Black
-      //     Colors.grey, // Darker shade of gray
-      //   ],
-      // ),
+      color: Colors.white,
       borderRadius: BorderRadius.circular(12),
     ),
-    child: Container(
-      margin: EdgeInsets.only(top: 95),
-      child: Row(
-        children: [
-          Card(
-            color: Colors.white.withOpacity(0.9),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0),
+    child: Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            height: 128,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: NetworkImage(
+                  scale: 1,
+                  "https://img.freepik.com/free-photo/business-job-interview-concept_1421-77.jpg",
+                ),
+                fit: BoxFit.cover,
+                // scale: 140,
+              ),
             ),
-            elevation: 4.0,
+          ),
+        ),
+        Expanded(
+            flex: 4,
             child: Container(
-              padding: const EdgeInsets.all(8),
+              height: 128,
+              // color: Colors.amber,
+              margin: EdgeInsets.symmetric(horizontal: 6),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    event.registartionDeadline.day.toString(),
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        announcement.title,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
-                  Text(
-                    event.registartionDeadline.monthShort,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                  Divider(
+                    indent: 16,
+                    color: Colors.black38,
+                    endIndent: 16,
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          announcement.publishedBy,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('MMM d, y')
+                              .format(announcement.publishedOn),
+                          style: TextStyle(
+                            fontSize: 8,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ],
                     ),
+                  )
+                ],
+              ),
+            ))
+      ],
+    ),
+  );
+}
+
+Widget EventItem(Event event, BuildContext context) {
+  return GestureDetector(
+    onTap: () => Navigator.pushNamed(context, EventDetailsScreen.routeName,
+        arguments: event),
+    child: Container(
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+          image: NetworkImage(
+            "https://images.unsplash.com/photo-1521048525864-2130780a12a5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          ),
+          fit: BoxFit.cover,
+        ),
+        // gradient: LinearGradient(
+        //   begin: Alignment.topLeft, // Gradient direction
+        //   end: Alignment.bottomRight,
+        //   colors: [
+        //     Colors.amberAccent, // Black
+        //     Colors.grey, // Darker shade of gray
+        //   ],
+        // ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Container(
+        margin: EdgeInsets.only(top: 95),
+        child: Row(
+          children: [
+            Card(
+              color: Colors.white.withOpacity(0.9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              elevation: 4.0,
+              child: Container(
+                padding:
+                    const EdgeInsets.all(7), //TODO: needs to make it responsive
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      event.registartionDeadline.day.toString(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      event.registartionDeadline.monthShort,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                //
+                children: [
+                  Text(
+                    event.eventName,
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              //
-              children: [
-                Text(
-                  event.eventName,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
