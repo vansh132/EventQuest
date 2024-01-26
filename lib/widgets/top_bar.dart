b55cbc3a6807f490e679b0eb9e724bc35efb1ea7
vansh132 <shah.vansh132@gmail.com>
2024-01-26 16:26:05 +0530

    topbar - colors updated

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 
 Widget TopBar() {
   return Container(
-    padding: EdgeInsets.all(16),
-    decoration: BoxDecoration(
-      color: Colors.redAccent,
+    padding: const EdgeInsets.all(16),
+    decoration: const BoxDecoration(
+      color: Color(0xff0D1B2A),
     ),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(
+        const Text(
           "EventQuest",
           style: TextStyle(
             fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
             fontFamily: 'Roboto-Black',
           ),
         ),
-        Text("christ logo"),
+        Image.asset(
+          scale: 1,
+          height: 48,
+          "assets/images/christ-logo-color.png",
+          fit: BoxFit.cover,
+        ),
       ],
     ),
   );
