9386f5e9fec06de7ea8c358cb7a74a7a4bf0a740
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-01-26 16:58:34 +0530

    add hero animation in the home screen

--- lib/screen_items/highlights.dart
+++ lib/screen_items/highlights.dart
@@ -3,36 +3,92 @@ import 'package:flutter/material.dart';
 List<String> images = [
   "https://images.unsplash.com/photo-1525921429624-479b6a26d84d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://plus.unsplash.com/premium_photo-1682284353470-53c3a913bded?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://img.freepik.com/free-vector/flat-illustration-back-school-season_23-2150599794.jpg?w=740&t=st=1706267291~exp=1706267891~hmac=c3c73e2c9eb34f4cbc57004f0ac546bc6e2b0d97d7e067cf45de01845ff1ea68",
+  "https://images.unsplash.com/photo-1523875194681-bedd468c58bf?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://img.freepik.com/free-photo/front-view-smiley-man-holding-book_23-2149915900.jpg?w=360&t=st=1706267307~exp=1706267907~hmac=7f9fb76e4341662680c8ecde9d05a1aa2226ead4509564c8a88f61a2f5bf3363",
 ];
 
-Widget Highlights() {
-  return Container(
-    height: 210,
-    padding: const EdgeInsets.symmetric(horizontal: 8),
-    // width: 300,
-    // color: Colors.cyanAccent,
-    child: ListView.builder(
-      itemCount: images.length,
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        return Container(
-          // height: 100,
-          width: 140,
-          margin: const EdgeInsets.symmetric(horizontal: 4),
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(12),
-            image: DecorationImage(
-              image: NetworkImage(
-                images[index],
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Image Gallery')),
+        body: Highlights(),
+      ),
+    );
+  }
+}
+
+class Highlights extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 210,
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: ListView.builder(
+        itemCount: images.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      FullScreenImage(imageUrl: images[index]),
+                ),
+              );
+            },
+            child: Hero(
+              tag: images[index],
+              child: Container(
+                width: 140,
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: NetworkImage(images[index]),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class FullScreenImage extends StatelessWidget {
+  final String imageUrl;
+
+  FullScreenImage({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: imageUrl,
+            child: Image.network(
+              imageUrl,
               fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height - 200,
+              width: MediaQuery.of(context).size.width,
             ),
           ),
-        );
-      },
-    ),
-  );
+        ),
+      ),
+    );
+  }
 }
