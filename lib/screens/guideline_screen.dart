f9af9e77899a10be9c5bad788de974c93c7f4a50
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-02-06 16:39:45 +0530

    Adding guideline screen for student

---
+++ lib/screens/guideline_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GuidelineScreen extends StatelessWidget {
+  const GuidelineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
