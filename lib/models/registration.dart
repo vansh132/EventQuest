d9a18c0bba61dbcd3992ab0fa23d2933f76a22fa
vansh <shah.vansh132@gmail.com>
2024-02-11 19:37:26 +0530

    registration model modified

--- lib/models/registration.dart
+++ lib/models/registration.dart
@@ -1,12 +1,18 @@
 class Registration {
   final String userId;
   final String eventId;
-  final List<String> teamDetails;
+  final List<TeamMember> teamDetails;
   final String paymentStatus;
 
   Registration(
-      {required this.userId,
+      {required this.userId, //whole object
       required this.eventId,
       required this.teamDetails,
       required this.paymentStatus});
+}
+
+class TeamMember {
+  final String name;
+
+  TeamMember({required this.name});
 }
