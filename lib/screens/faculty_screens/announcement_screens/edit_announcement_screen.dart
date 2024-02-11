d0fba4a50af00143f55e23a09d716d1f016dd783
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-02-11 19:30:30 +0530

    Error Solved

--- lib/screens/faculty_screens/announcement_screens/edit_announcement_screen.dart
+++ lib/screens/faculty_screens/announcement_screens/edit_announcement_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
 import 'package:eventquest/models/announcement.dart';
 import 'package:eventquest/providers/announcement_notifier.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -20,6 +24,38 @@ class _EditAnnouncementScreenState
   late TextEditingController description;
   late TextEditingController publishBy;
   late Announcement announcementData;
+
+  File image = File("");
+  bool submitted = false;
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      image = res;
+    });
+  }
+
+  Future<File> pickImages() async {
+    File image = File("");
+    try {
+      var files = await FilePicker.platform.pickFiles(
+        type: FileType.image,
+        allowMultiple: true,
+      );
+
+      if (files != null && files.files.isNotEmpty) {
+        image = File(files.files[0].path!);
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return image;
+  }
+
+  void clearImage() {
+    setState(() {
+      image = File("");
+    });
+  }
 
   @override
   void didChangeDependencies() {
@@ -118,10 +154,63 @@ class _EditAnnouncementScreenState
                 const SizedBox(
                   height: 16,
                 ),
-                //TODO: add update file field
-                const Text(
-                  "Upload File yet to be added",
-                  style: TextStyle(color: Colors.red),
+                image.existsSync() == true
+                    ? Center(
+                        child: SizedBox(
+                          height: 200,
+                          width: 200,
+                          child: Image(
+                            image: FileImage(image),
+                          ),
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: selectImages,
+                        child: DottedBorder(
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 4],
+                          borderType: BorderType.RRect,
+                          strokeCap: StrokeCap.round,
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.folder_open_outlined,
+                                  size: 40,
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  "Upload Announcement Images",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: submitted == true ? null : clearImage,
+                      label: const Text(
+                        "Clear",
+                      ),
+                      icon: const Icon(Icons.cancel_outlined),
+                    ),
+                  ],
                 ),
                 const Text(
                   "Publish By",
