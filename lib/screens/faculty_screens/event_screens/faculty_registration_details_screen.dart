01d19bff26042e864660445e4dc060a536763fd5
vansh <shah.vansh132@gmail.com>
2024-02-12 23:43:15 +0530

    registration screen setup ready

--- lib/screens/faculty_screens/event_screens/faculty_registration_details_screen.dart
+++ lib/screens/faculty_screens/event_screens/faculty_registration_details_screen.dart
@@ -5,6 +5,10 @@ class RegistrationDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("Screen is yet to be built"),
+      ),
+    );
   }
 }
