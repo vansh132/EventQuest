f2374d37c47b5700f549c7c65770e4362142857e
vansh <shah.vansh132@gmail.com>
2024-02-15 15:13:44 +0530

    server setup done

---
+++ servers/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = 3000
+const DB = "mongodb+srv://vansh:vansh@eventquest.p0fpk2f.mongodb.net/?retryWrites=true&w=majority"
+
+app.use(express.json());
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Connected at port " + PORT);
+});
