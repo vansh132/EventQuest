7e15aee36de395b8d46b2338b5f335047c27f933
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-03-08 23:41:16 +0530

    Dynamic home screen

--- lib/screens/faculty_screens/faculty_home_screen.dart
+++ lib/screens/faculty_screens/faculty_home_screen.dart
@@ -1,26 +1,47 @@
 import 'package:eventquest/models/announcement.dart';
 import 'package:eventquest/models/event.dart';
-import 'package:eventquest/screen_items/faculty_custom_navigation_bottom_bar.dart';
 import 'package:eventquest/screen_items/highlights.dart';
 import 'package:eventquest/screens/student_screens/announcement_screens/announcement_detail_screen.dart';
 import 'package:eventquest/screens/student_screens/event_screens/event_detail_screen.dart';
+import 'package:eventquest/services/announcement_services.dart';
+import 'package:eventquest/services/event_services.dart';
 import 'package:eventquest/widgets/top_bar.dart';
 import 'package:eventquest/widgets/user_info.dart';
 import 'package:flutter/material.dart';
 
-class FacultyHomeScreen extends StatelessWidget {
+class FacultyHomeScreen extends StatefulWidget {
   static const String routeName = 'home-screen';
   const FacultyHomeScreen({super.key});
 
   @override
+  State<FacultyHomeScreen> createState() => _FacultyHomeScreenState();
+}
+
+class _FacultyHomeScreenState extends State<FacultyHomeScreen> {
+  List<Event> events = [];
+  List<Announcement> announcements = [];
+  Future<List<Event>> getAllPost() async {
+    EventServices eventServices = EventServices();
+    events = await eventServices.getAllEvents(context);
+    return events;
+  }
+
+  Future<List<Announcement>> getAllAnnouncement() async {
+    AnnouncementServices announcementServices = AnnouncementServices();
+    announcements = await announcementServices.getAllAnnouncements(context);
+
+    return announcements;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getAllPost();
+    getAllAnnouncement();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<Announcement> announcements = [
-
-    ];
-    List<Event> events = [
-
-    ];
-
     return Scaffold(
       body: Container(
         // color: Color(0xff0D1B2A),
@@ -41,111 +62,114 @@ class FacultyHomeScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget RecentAnnouncements(
-    BuildContext context, List<Announcement> announcements) {
-  return Container(
-    width: MediaQuery.of(context).size.width,
-    padding: const EdgeInsets.all(16),
-    // color: Colors.greenAccent,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          "Recent Announcements",
-          style: TextStyle(
-            fontSize: 24,
-            color: Color(0xff0D1B2A),
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.2,
-          ),
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        Container(
-          // color: Colors.greenAccent,
-          height: 200,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3), // Shadow color
-                spreadRadius: 5, // Spread radius
-                blurRadius: 7, // Blur radius
-                offset: const Offset(0, 3), // Offset in the x, y direction
-              ),
-            ],
-          ),
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: announcements.length,
-            itemBuilder: (context, index) =>
-                AnnouncementItem(context, announcements[index]),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 16,
-          ),
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(
-                    context,
-                    FacultyCustomBottomBar.routeName,
-                  );
-                },
-                child: Text(
-                  "Faculty - Home Screen",
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
-    ),
-  );
-}
-
-Widget recentEvents(BuildContext context, List<Event> events) {
-  return Container(
-    width: MediaQuery.of(context).size.width,
-    padding: const EdgeInsets.all(16),
-    // color: Colors.deepOrange,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          "Recent Events",
-          style: TextStyle(
-            fontSize: 24,
-            color: Color(0xff0D1B2A),
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.2,
-          ),
-        ),
-        const SizedBox(
-          height: 6,
-        ),
-        SizedBox(
-          // color: Colors.greenAccent,
-          height: 200,
-          child: GridView.builder(
-            scrollDirection: Axis.horizontal,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1,
-              crossAxisSpacing: 4.0,
-              mainAxisSpacing: 4.0,
+  Widget RecentAnnouncements(
+      BuildContext context, List<Announcement> announcements) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.all(16),
+      // color: Colors.greenAccent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Recent Announcements",
+            style: TextStyle(
+              fontSize: 24,
+              color: Color(0xff0D1B2A),
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.2,
             ),
-            itemCount: events.length,
-            itemBuilder: (context, index) => EventItem(events[index], context),
           ),
-        ),
-      ],
-    ),
-  );
+          const SizedBox(
+            height: 12,
+          ),
+          Container(
+            // color: Colors.greenAccent,
+            height: 200,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3), // Shadow color
+                  spreadRadius: 5, // Spread radius
+                  blurRadius: 7, // Blur radius
+                  offset: const Offset(0, 3), // Offset in the x, y direction
+                ),
+              ],
+            ),
+            child: FutureBuilder(
+                future: getAllAnnouncement(),
+                initialData: announcements,
+                builder: (context, snapshot) {
+                  if (snapshot.data!.length > 5) {
+                    snapshot.data!.length = 5;
+                  } else {
+                    snapshot.data!.length = snapshot.data!.length;
+                  }
+
+                  return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) =>
+                        AnnouncementItem(context, snapshot.data![index]),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget recentEvents(BuildContext context, List<Event> events) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.all(16),
+      // color: Colors.deepOrange,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Recent Events",
+            style: TextStyle(
+              fontSize: 24,
+              color: Color(0xff0D1B2A),
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.2,
+            ),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          SizedBox(
+            // color: Colors.greenAccent,
+            height: 200,
+            child: FutureBuilder(
+                future: getAllPost(),
+                initialData: events,
+                builder: (context, snapshot) {
+                  if (snapshot.data!.length > 5) {
+                    snapshot.data!.length = 5;
+                  } else {
+                    snapshot.data!.length = snapshot.data!.length;
+                  }
+                  return GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      crossAxisSpacing: 4.0,
+                      mainAxisSpacing: 4.0,
+                    ),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) =>
+                        EventItem(snapshot.data![index], context),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 Widget AnnouncementItem(BuildContext context, Announcement announcement) {
@@ -170,7 +194,7 @@ Widget AnnouncementItem(BuildContext context, Announcement announcement) {
               width: 100, // Set the desired width
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(12.0),
-                child: Image.asset(
+                child: Image.network(
                   announcement.announcementImages![0],
                   fit: BoxFit.cover,
                 ),
@@ -218,7 +242,7 @@ Widget AnnouncementItem(BuildContext context, Announcement announcement) {
                             ),
                           ),
                           Text(
-                            announcement.announcementPublishedOn,
+                            announcement.announcementPublishedOn.toString(),
                             style: const TextStyle(
                               fontSize: 8,
                               fontWeight: FontWeight.w500,
@@ -238,6 +262,7 @@ Widget AnnouncementItem(BuildContext context, Announcement announcement) {
 }
 
 Widget EventItem(Event event, BuildContext context) {
+  var date = event.eventRegistrationDeadline.split("T")[0];
   return GestureDetector(
     onTap: () => Navigator.pushNamed(context, EventDetailsScreen.routeName,
         arguments: event),
@@ -250,7 +275,7 @@ Widget EventItem(Event event, BuildContext context) {
           width: double.infinity, // Set the desired width
           child: ClipRRect(
             borderRadius: BorderRadius.circular(12.0),
-            child: Image.asset(
+            child: Image.network(
               event.eventImage,
               fit: BoxFit.cover,
             ),
@@ -259,63 +284,33 @@ Widget EventItem(Event event, BuildContext context) {
         Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Container(
-              // margin: const EdgeInsets.only(top: 95),
-              child: Row(
-                children: [
-                  Card(
-                    color: Colors.white.withOpacity(0.9),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    elevation: 4.0,
-                    child: Container(
-                      padding: const EdgeInsets.all(
-                          7), //TODO: needs to make it responsive
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            event.eventRegistrationDeadline.toString(),
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            event.eventRegistrationDeadline.toString(),
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
+            Card(
+              color: Colors.white.withOpacity(0.9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 4.0,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      date.split("-")[2],
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(4),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              color: Colors.black54),
-                          child: Text(
-                            event.eventName,
-                            style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      month(int.parse(date.split("-")[1])),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
@@ -325,22 +320,36 @@ Widget EventItem(Event event, BuildContext context) {
   );
 }
 
-extension MonthToString on DateTime {
-  String get monthShort {
-    return [
-      '',
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
-    ][month];
+String month(int? index) {
+  switch (index) {
+    case 00:
+      return '';
+    case 01:
+      return 'Jan';
+    case 02:
+      return 'Feb';
+    case 03:
+      return 'Mar';
+    case 04:
+      return 'Apr';
+    case 05:
+      return 'May';
+    case 06:
+      return 'Jun';
+    case 07:
+      return 'Jul';
+    case 08:
+      return 'Aug';
+    case 09:
+      return 'Sep';
+    case 10:
+      return 'Oct';
+    case 11:
+      return 'Nov';
+    case 12:
+      return 'Dec';
+
+    default:
+      return '';
   }
 }
