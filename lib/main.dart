7e3c7ed03531888a00d8b97930189bd1ddbd716b
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-03-11 15:52:03 +0530

    Fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: ThemeData(fontFamily: 'Roboto'),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Provider.of<UserProvider>(context).user.type == "Student"
           ? CustomBottomNavigationBar()
