286d1b5de2e700509d07f590ae652b191a29959a
Riya Shah <riyashah@Riyas-MacBook-Air-3.local>
2024-03-12 10:42:45 +0530

    Remove warnings

--- lib/screen_items/highlights.dart
+++ lib/screen_items/highlights.dart
@@ -1,14 +1,5 @@
-import 'dart:convert';
-
 import 'package:eventquest/services/task_services.dart';
 import 'package:flutter/material.dart';
-
-// List<String> images = [
-//   "https://images.unsplash.com/photo-1525921429624-479b6a26d84d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "https://plus.unsplash.com/premium_photo-1682284353470-53c3a913bded?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "https://images.unsplash.com/photo-1523875194681-bedd468c58bf?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "https://img.freepik.com/free-photo/front-view-smiley-man-holding-book_23-2149915900.jpg?w=360&t=st=1706267307~exp=1706267907~hmac=7f9fb76e4341662680c8ecde9d05a1aa2226ead4509564c8a88f61a2f5bf3363",
-// ];
 
 void main() {
   runApp(const MyApp());
