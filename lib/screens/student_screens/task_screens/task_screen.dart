cee118ac662c02dfbb72bb32f7fdfe00d2766db5
Vansh Shah <shah.vansh132@gmail.com>
2024-08-02 11:11:35 +0530

    task screen (student): UI redesigned

--- lib/screens/student_screens/task_screens/task_screen.dart
+++ lib/screens/student_screens/task_screens/task_screen.dart
@@ -15,27 +15,7 @@ class TaskScreen extends StatefulWidget {
 class _TaskScreenState extends State<TaskScreen> {
   bool selected = true;
 
-  List<Task> assignedTasks = [
-    // Task(
-    //   taskTitle: "Talent Show",
-    //   taskDescription:
-    //       "Continuing our tradition, we are pleased to announce the forthcoming Talent Show scheduled for February 20th, 2024. This esteemed event will take place at 911, Central Block, providing an esteemed platform for our fresher students to exhibit their talents. The Talent Show serves as an avenue for showcasing diverse skills and abilities, fostering a culture of creativity and expression within our academic community. ",
-    //   taskType: "Poster",
-    //   assignedTo: "2347152",
-    //   assignedBy: "Helen K Joy",
-    //   taskStatus: false,
-    // ),
-    // Task(
-    //   taskTitle: "Talent Show",
-    //   taskDescription:
-    //       "Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it Create poster for it",
-    //   taskType: "Poster",
-    //   assignedTo: "2347152",
-    //   assignedBy: "Helen K Joy",
-    //   taskStatus: true,
-    //   remarks: ["feefef"],
-    // ),
-  ];
+  List<Task> assignedTasks = [];
 
   TaskServices taskServices = TaskServices();
 
@@ -52,20 +32,11 @@ class _TaskScreenState extends State<TaskScreen> {
   @override
   void initState() {
     super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
     getAllTask();
-    // TODO: implement didChangeDependencies
   }
 
   @override
   Widget build(BuildContext context) {
-    print(assignedTasks);
-    print(assignedTasks.isEmpty.toString() + "dede");
-    print(selected);
     final appColors = context.appColors;
 
     ButtonStyle selectedButtonStyle = ButtonStyle(
@@ -89,19 +60,26 @@ class _TaskScreenState extends State<TaskScreen> {
         Theme.of(context).textTheme.titleSmall as TextStyle;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: appColors.white,
-        title: Text(
-          "Manage Your Tasks",
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: appColors.white,
+      //   title: Text(
+      //     "Manage Your Tasks",
+      //     style: Theme.of(context).textTheme.displayMedium,
+      //   ),
+      // ),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(8),
           width: double.infinity,
           child: Column(
             children: [
+              Text(
+                "Manage Your Tasks",
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
               Text(
                 "\"The harder you work for something, the greater you'll feel when you achieve it.\"",
                 style: GoogleFonts.raleway(
@@ -159,61 +137,47 @@ class _TaskScreenState extends State<TaskScreen> {
                   ),
                 ],
               ),
-              assignedTasks.isEmpty
-                  ? Center(
-                      child: Text("data"),
-                    )
-                  : Container(
-                      margin: const EdgeInsets.all(8),
-                      padding: const EdgeInsets.all(16),
-                      // color: Colors.greenAccent,
-                      height: 400,
-                      child: FutureBuilder(
-                        future: getAllTask(),
-                        initialData: assignedTasks,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            return ListView.builder(
-                              itemBuilder: (context, index) =>
-                                  TaskItem(snapshot.data![index], context),
-                              itemCount: snapshot.data!.length,
-                            );
-                          } else if (snapshot.data == null) {
-                            return const Center(
-                              child: Text("No data found"),
-                            );
-                          } else {
-                            return const CircularProgressIndicator();
-                          }
-                        },
-                      ),
-                    ),
-
-              // Container(
-              //   margin: const EdgeInsets.all(8),
-              //   padding: const EdgeInsets.all(16),
-              //   // color: Colors.greenAccent,
-              //   height: 400,
-              //   child: FutureBuilder(
-              //     future: getAllTask(),
-              //     initialData: assignedTasks,
-              //     builder: (context, snapshot) {
-              //       if (snapshot.hasData) {
-              //         return ListView.builder(
-              //           itemBuilder: (context, index) =>
-              //               TaskItem(snapshot.data![index], context),
-              //           itemCount: snapshot.data!.length,
-              //         );
-              //       } else if (snapshot.data == null) {
-              //         return const Center(
-              //           child: Text("No data found"),
-              //         );
-              //       } else {
-              //         return const CircularProgressIndicator();
-              //       }
-              //     },
-              //   ),
-              // ),
+              Container(
+                // width: 500,
+                // margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                // color: Colors.greenAccent,
+                height: 400,
+                child: FutureBuilder(
+                  future: getAllTask(),
+                  initialData: assignedTasks,
+                  builder: (context, snapshot) {
+                    if (snapshot.data!.isEmpty) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/images/task.gif",
+                            height: 72,
+                            width: 72,
+                          ),
+                          Text(
+                            "All tasks completed!",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          Text(
+                            "Take this moment to relax and recharge!",
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                        ],
+                      );
+                    } else if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemBuilder: (context, index) =>
+                            taskItem(snapshot.data![index], context),
+                        itemCount: snapshot.data!.length,
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -222,7 +186,7 @@ class _TaskScreenState extends State<TaskScreen> {
   }
 }
 
-Widget TaskItem(Task task, BuildContext context) {
+Widget taskItem(Task task, BuildContext context) {
   return GestureDetector(
     onTap: () => Navigator.pushNamed(
       context,
@@ -230,93 +194,35 @@ Widget TaskItem(Task task, BuildContext context) {
       arguments: task,
     ),
     child: Container(
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      margin: const EdgeInsets.symmetric(
+        vertical: 8,
+      ),
       decoration: BoxDecoration(
-        color: Colors.white60,
+        color: Colors.white,
+        border: Border.all(width: 0.2),
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.3), // Shadow color
-            spreadRadius: 5, // Spread radius
-            blurRadius: 7, // Blur radius
-            offset: const Offset(0, 3), // Offset in the x, y direction
+            spreadRadius: 1, // Spread radius
+            blurRadius: 3, // Blur radius
+            offset: const Offset(2, 2), // Offset in the x, y direction
           ),
         ],
       ),
       child: ListTile(
         title: Text(
           task.taskTitle,
-          style: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1,
-          ),
+          style: Theme.of(context).textTheme.titleMedium,
         ),
-        subtitle: Column(
-          children: [
-            const Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: "Type: ",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                        TextSpan(
-                          text: task.taskType,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: "Status: ",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                        TextSpan(
-                          text: task.taskStatus ? "Completed" : "Incompleted",
-                          style: !task.taskStatus
-                              ? const TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                )
-                              : const TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        subtitle: Text(
+          "Assigned By: ${task.assignedBy}",
+          style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+                color: Colors.blueGrey.withOpacity(0.8),
+              ),
         ),
       ),
     ),
