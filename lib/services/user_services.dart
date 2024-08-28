53a96b2df877bd6e9555f7446054cf3a115f89ad
Vansh Shah <shah.vansh132@gmail.com>
2024-08-28 19:57:28 +0530

    user login: error cases handling bugs fixed

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,7 +1,7 @@
-import 'package:event_quest/constants/error_handling.dart';
 import 'package:event_quest/constants/global_variable.dart';
 import 'package:event_quest/provider/user_provider.dart';
 import 'package:event_quest/screens/constants/utils.dart';
+import 'package:event_quest/theme/theme_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
@@ -15,9 +15,48 @@ class UserService {
       customSnackbar(context, "Error", errorMessage);
     }
 
-    void onSuccess(http.Response res) {
+    void onSuccessOperation(http.Response res) {
+      customSnackbar(context, "Success", "Authentication successful");
+
       Provider.of<UserProvider>(context, listen: false).setUser(res.body);
       Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
+    }
+
+    void onSuccess(http.Response res) async {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          final appColors = context.appColors;
+          return Dialog(
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircularProgressIndicator(
+                    color: appColors.primary,
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Authenticating...",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+      await Future.delayed(const Duration(seconds: 2));
+      onSuccessOperation(res);
     }
 
     try {
@@ -29,10 +68,8 @@ class UserService {
         'Content-Type': 'application/json; charset=UTF-8'
       });
 
-      print(res.statusCode);
-
       if (res.statusCode == 404) {
-        const errorMessage = "User not found";
+        const errorMessage = "Invalid username";
         handleHttpError(errorMessage);
         return;
       } else if (res.statusCode == 401) {
